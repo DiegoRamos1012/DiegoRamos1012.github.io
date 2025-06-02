bfebc7136577e5b8a29e8cb8d1401d5c6fc6cee6
diego1012 <diegorsantos09@outlook.com>
2025-06-02 08:42:39 -0300

    Update deployment configuration and remove unnecessary script for GitHub Pages

--- Portfolio/vite.config.ts
+++ Portfolio/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Use o nome do seu repositório aqui
+  base: '/DiegoRamos1012.github.io/', // Use o nome do seu repositório aqui
 })
